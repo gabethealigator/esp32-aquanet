800717d2ceeaa431d5540335ffba11f9b84b89d2
gabrielmslima <gabrielnunesinfor@gmail.com>
2024-08-11 17:26:33 -0300

    Authentication with custom parameters working and data is being sent

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@
 #include <addons/TokenHelper.h>
 #include <Firebase_ESP_Client.h>
 
-#define API_KEY ""
-#define DATABASE_URL ""
-#define USER_EMAIL ""
-#define USER_PASSWORD ""
+#define API_KEY "APIKEY"
+#define DATABASE_URL "DATABASEURL"
 
 #define SETUP_PIN 0
 #define LED 2
@@ -19,16 +17,17 @@ FirebaseConfig config;
 
 bool connected = false;
 unsigned long previousMillis = 0;
+unsigned long dataMillis = 0;
 const long interval = 1000;
+int count = 0;
 
 class FireBaseManager {
 private:
-    unsigned long sendDataPreviousMillis = 0;
     bool signupOK = false;
 
 public:
-    bool checkAuthCredentials() {
-        signupOK = Firebase.signUp(&config, &auth, USER_EMAIL, USER_PASSWORD);
+    bool checkAuthCredentials(String email, String password) {
+        signupOK = Firebase.signUp(&config, &auth, email.c_str(), password.c_str());
         return true;
     }
 };
@@ -43,6 +42,11 @@ void ConnectToWifi() {
         connected = false;
     }
 }
+
+struct Settings {
+  char email[60];
+  char password[20];
+} settings;
 
 FireBaseManager firebaseManager;
 
@@ -63,6 +67,10 @@ void setup() {
         delay(500);
     }
 
+    EEPROM.begin(512);
+    EEPROM.get(0, settings);
+    Serial.println("Settings loaded from EEPROM"); 
+
     // Check if the setup button is pressed
     if (digitalRead(SETUP_PIN) == LOW) {
         Serial.println("Entering setup mode");
@@ -77,7 +85,30 @@ void setup() {
         // Start the wifi manager to configure the wifi
         WiFiManager wm;
 
+        settings.password[19] = '\0';
+        settings.email[59] = '\0';
+
+        WiFiManagerParameter email_parameter("email", "Email", settings.email, 60);
+        WiFiManagerParameter password_parameter("password", "Password", settings.password, 20, "type='password'");
+
+        wm.addParameter(&email_parameter);
+        wm.addParameter(&password_parameter);
+
         wm.startConfigPortal("AquaNet");
+
+        strcpy(settings.email, email_parameter.getValue());
+        strcpy(settings.password, password_parameter.getValue());
+
+        EEPROM.put(0, settings);
+        if (EEPROM.commit())
+        {
+          Serial.println("Settings saved to EEPROM");
+        }
+        else
+        {
+          Serial.println("Error saving settings to EEPROM");
+        }
+
     } 
     if (digitalRead(SETUP_PIN) == HIGH) {
         Serial.println("Starting the device");
@@ -85,8 +116,8 @@ void setup() {
 
         config.api_key = API_KEY;
         config.database_url = DATABASE_URL;
-        auth.user.email = USER_EMAIL;
-        auth.user.password = USER_PASSWORD;
+        auth.user.email = settings.email;
+        auth.user.password = settings.password;
 
         config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
         Firebase.begin(&config, &auth);
@@ -95,7 +126,7 @@ void setup() {
         fbdo.setBSSLBufferSize(4096, 1024);
         fbdo.setResponseSize(4096);
         
-        bool checkAuthCredentials = firebaseManager.checkAuthCredentials();
+        bool checkAuthCredentials = firebaseManager.checkAuthCredentials(settings.email, settings.password);
         if (checkAuthCredentials) {
           Serial.println("Auth credentials are OK");
         }
@@ -105,9 +136,9 @@ void setup() {
     }
 
     Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+    Serial.println("Email: " + String(settings.email));
+    Serial.println("Password: " + String(settings.password));
 }
-
-int TEMP_TEST = 0;
 
 void loop() {
     ConnectToWifi();
@@ -119,5 +150,12 @@ void loop() {
             delay(100);
             digitalWrite(LED, LOW);
         }
+    }
+    // TODO: Add the pairing code logic
+    if (millis() - dataMillis > 5000 && Firebase.ready())
+    {
+        dataMillis = millis();
+        String path = "/UsersData/4422/TEMP";
+        Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, path, count++) ? "ok" : fbdo.errorReason().c_str());
     }
 }
