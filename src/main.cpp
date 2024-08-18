e1238e42d82d1353757013a08bca31923eba5385
gabrielmslima <gabrielnunesinfor@gmail.com>
2024-08-18 00:05:59 -0300

    Add device unique id to path

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include <WiFiManager.h>
+#include "esp_system.h"
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <Firebase_ESP_Client.h>
+#include <WiFiManager.h>
 #include <addons/RTDBHelper.h>
 #include <addons/TokenHelper.h>
-#include <Firebase_ESP_Client.h>
-#include <random>
 
 #define API_KEY "AIzaSyBhj3If5etw9wk-QXnNnU0vvRxBKk2syFw"
 #define DATABASE_URL "https://site-aqua-54d76-default-rtdb.firebaseio.com"
@@ -26,173 +26,175 @@ const unsigned long firebaseInterval = 5000;
 
 class FireBaseManager {
 private:
-    bool signupOK = false;
+  bool signupOK = false;
 
 public:
-    bool checkAuthCredentials(String email, String password) {
-        signupOK = Firebase.signUp(&config, &auth, email.c_str(), password.c_str());
-        return signupOK;
-    }
+  bool checkAuthCredentials(String email, String password) {
+    signupOK = Firebase.signUp(&config, &auth, email.c_str(), password.c_str());
+    return signupOK;
+  }
 };
 
 void ConnectToWifi() {
-    if (WiFi.status() != WL_CONNECTED) {
-        WiFi.begin();
-        if (WiFi.waitForConnectResult() == WL_CONNECTED) {
-            connected = true;
-            Serial.println("Wi-Fi connected");
-        }
-        if (WiFi.status() != WL_CONNECTED) {
-            Serial.println("Wi-Fi connection failed");
-        }
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin();
+    if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+      connected = true;
+      Serial.println("Wi-Fi connected");
     }
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Wi-Fi connection failed");
+    }
+  }
 }
 
-String PairingCodeGenerator() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(1000, 9999);
-
-    int code = dis(gen);
-    return String(code);
+String GetDeviceUniqueId() {
+  uint32_t chipId = ESP.getEfuseMac();
+  return String(chipId, HEX);
 }
 
 struct Settings {
-    char email[60];
-    char password[20];
-    char pairingCode[5];
+  char email[60];
+  char password[20];
+  char DeviceUniqueId[5];
 } settings;
 
 FireBaseManager firebaseManager;
 
 void setup() {
-    Serial.begin(9600);
-    WiFi.mode(WIFI_STA);
-    pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  pinMode(LED, OUTPUT);
 
-    Serial.println("Press the button to enter setup mode");
-    for (int i = 3; i > 0; i--) {
-        Serial.print(i);
-        Serial.print("...");
-        digitalWrite(LED, HIGH);
-        delay(500);
-        digitalWrite(LED, LOW);
-        delay(500);
+  Serial.println("Press the button to enter setup mode");
+  for (int i = 3; i > 0; i--) {
+    Serial.print(i);
+    Serial.print("...");
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+  }
+
+  EEPROM.begin(512);
+  EEPROM.get(0, settings);
+  Serial.println("Settings loaded from EEPROM");
+
+  if (digitalRead(SETUP_PIN) == LOW) {
+    Serial.println("Entering setup mode");
+
+    for (int i = 0; i < 2; i++) {
+      digitalWrite(LED, HIGH);
+      delay(300);
+      digitalWrite(LED, LOW);
+      delay(300);
     }
 
-    EEPROM.begin(512);
-    EEPROM.get(0, settings);
-    Serial.println("Settings loaded from EEPROM");
+    WiFiManager wm;
 
-    if (digitalRead(SETUP_PIN) == LOW) {
-        Serial.println("Entering setup mode");
+    String DeviceUniqueId = GetDeviceUniqueId();
+    DeviceUniqueId.toCharArray(settings.DeviceUniqueId,
+                               sizeof(settings.DeviceUniqueId));
 
-        for (int i = 0; i < 2; i++) {
-            digitalWrite(LED, HIGH);
-            delay(300);
-            digitalWrite(LED, LOW);
-            delay(300);
-        }
+    settings.password[19] = '\0';
+    settings.email[59] = '\0';
 
-        WiFiManager wm;
+    WiFiManagerParameter email_parameter("email", "Email", settings.email, 60);
+    WiFiManagerParameter password_parameter(
+        "password", "Password", settings.password, 20, "type='password'");
 
-        String pairingCode = PairingCodeGenerator();
-        pairingCode.toCharArray(settings.pairingCode, sizeof(settings.pairingCode));
+    wm.addParameter(&email_parameter);
+    wm.addParameter(&password_parameter);
 
-        settings.password[19] = '\0';
-        settings.email[59] = '\0';
+    wm.startConfigPortal("AquaNet");
 
-        WiFiManagerParameter email_parameter("email", "Email", settings.email, 60);
-        WiFiManagerParameter password_parameter("password", "Password", settings.password, 20, "type='password'");
+    strcpy(settings.email, email_parameter.getValue());
+    strcpy(settings.password, password_parameter.getValue());
 
-        wm.addParameter(&email_parameter);
-        wm.addParameter(&password_parameter);
-
-        wm.startConfigPortal("AquaNet");
-
-        strcpy(settings.email, email_parameter.getValue());
-        strcpy(settings.password, password_parameter.getValue());
-
-        EEPROM.put(0, settings);
-        if (EEPROM.commit()) {
-            Serial.println("Settings saved to EEPROM");
-        }
-        if (!EEPROM.commit()) {
-            Serial.println("Error saving settings to EEPROM");
-        }
+    EEPROM.put(0, settings);
+    if (EEPROM.commit()) {
+      Serial.println("Settings saved to EEPROM");
     }
-
-    Serial.println("Starting the device");
-    ConnectToWifi();
-
-    config.api_key = API_KEY;
-    config.database_url = DATABASE_URL;
-    auth.user.email = settings.email;
-    auth.user.password = settings.password;
-
-    config.token_status_callback = tokenStatusCallback;
-    Firebase.begin(&config, &auth);
-    Firebase.reconnectNetwork(true);
-
-    fbdo.setBSSLBufferSize(4096, 1024);
-    fbdo.setResponseSize(4096);
-
-    bool checkAuthCredentials = firebaseManager.checkAuthCredentials(settings.email, settings.password);
-    if (checkAuthCredentials) {
-        Serial.println("Auth credentials are OK");
+    if (!EEPROM.commit()) {
+      Serial.println("Error saving settings to EEPROM");
     }
-    if (!checkAuthCredentials) {
-        Serial.println("Auth credentials are not OK");
-    }
+  }
 
-    Firebase.RTDB.beginStream(&fbdoStream, "/UsersData/" + String(settings.pairingCode));
+  Serial.println("Starting the device");
+  ConnectToWifi();
 
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-    Serial.println("Email: " + String(settings.email));
-    Serial.println("Password: " + String(settings.password));
-    Serial.println("Pairing Code: " + String(settings.pairingCode));
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = settings.email;
+  auth.user.password = settings.password;
+
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectNetwork(true);
+
+  fbdo.setBSSLBufferSize(4096, 1024);
+  fbdo.setResponseSize(4096);
+
+  bool checkAuthCredentials =
+      firebaseManager.checkAuthCredentials(settings.email, settings.password);
+  if (checkAuthCredentials) {
+    Serial.println("Auth credentials are OK");
+  }
+  if (!checkAuthCredentials) {
+    Serial.println("Auth credentials are not OK");
+  }
+
+  Firebase.RTDB.beginStream(&fbdoStream,
+                            "/UsersData/" + String(settings.DeviceUniqueId));
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  Serial.println("Email: " + String(settings.email));
+  Serial.println("Password: " + String(settings.password));
+  Serial.println("Pairing Code: " + String(settings.DeviceUniqueId));
 }
 
 void loop() {
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
 
-    if (WiFi.status() != WL_CONNECTED) {
-        ConnectToWifi();
+  if (WiFi.status() != WL_CONNECTED) {
+    ConnectToWifi();
+  }
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    digitalWrite(LED, !digitalRead(LED));
+  }
+
+  if (Firebase.ready() &&
+      currentMillis - lastFirebaseOperation >= firebaseInterval) {
+    lastFirebaseOperation = currentMillis;
+
+    FirebaseJson json;
+    json.set("TEMP", count++);
+    json.set("PH", count++);
+    json.set("LEVEL", count++);
+    json.set("timestamp", currentMillis);
+
+    String path = "/UsersData/";
+    path += auth.token.uid.c_str();
+    path += "/";
+    path += settings.DeviceUniqueId;
+    if (Firebase.RTDB.setJSON(&fbdo, path, &json)) {
+      Serial.println("Data sent successfully");
     }
-
-    if (currentMillis - previousMillis >= interval) {
-        previousMillis = currentMillis;
-        digitalWrite(LED, !digitalRead(LED));
+    if (!Firebase.RTDB.setJSON(&fbdo, path, &json)) {
+      Serial.printf("Error sending data: %s\n", fbdo.errorReason().c_str());
     }
+  }
 
-    if (Firebase.ready() && currentMillis - lastFirebaseOperation >= firebaseInterval) {
-        lastFirebaseOperation = currentMillis;
-
-        FirebaseJson json;
-        json.set("TEMP", count++);
-        json.set("PH", count++);
-        json.set("LEVEL", count++);
-        json.set("timestamp", currentMillis);
-
-        String path = "/UsersData/";
-        path += auth.token.uid.c_str();
-        path += "/";
-        path += settings.pairingCode;
-        if (Firebase.RTDB.setJSON(&fbdo, path, &json)) {
-            Serial.println("Data sent successfully");
-        }
-        if (!Firebase.RTDB.setJSON(&fbdo, path, &json)) {
-            Serial.printf("Error sending data: %s\n", fbdo.errorReason().c_str());
-        }
+  if (Firebase.ready() && Firebase.RTDB.readStream(&fbdoStream)) {
+    if (fbdoStream.streamAvailable()) {
+      String path = fbdoStream.streamPath();
+      String data = fbdoStream.stringData();
+      Serial.printf("Stream data available at path: %s, data: %s\n",
+                    path.c_str(), data.c_str());
     }
+  }
 
-    if (Firebase.ready() && Firebase.RTDB.readStream(&fbdoStream)) {
-        if (fbdoStream.streamAvailable()) {
-            String path = fbdoStream.streamPath();
-            String data = fbdoStream.stringData();
-            Serial.printf("Stream data available at path: %s, data: %s\n", path.c_str(), data.c_str());
-        }
-    }
-
-    yield();
+  yield();
 }
